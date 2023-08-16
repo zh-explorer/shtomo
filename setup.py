09f30688676e9c2f6efb366c228ee940d50d8f47
explorer <hsadkhk@gmail.com>
2023-08-16 15:36:49 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='shtomo',
+    version='0.0.1',
+    packages=['shtomo'],
+    url='https://github.com/zh-explorer/shtomo',
+    license='WTFPL',
+    author='explorer',
+    author_email='hsadkhk@gmail.com',
+    description='',
+    install_requires=[
+        "cmd2",
+    ],
+    python_requires='>=3, <4',
+)
